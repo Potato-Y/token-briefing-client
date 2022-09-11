a13ff83c562524c4742bad04adac7dac9b75f307
Potato-Y <projectpotatoy@gmail.com>
2022-09-11 18:24:51 +0900

    기본 구성
    - 타이틀 바 추가
    - 레이아웃 구성

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div id="head-box">
+      <span
+        id="navlink-home"
+        class="navlink-to-reset font-background-transparency"
+        style="cursor: pointer"
+      >
+        Token Briefing
+      </span>
+
+      &nbsp;&nbsp;|&nbsp;&nbsp;
+      <!-- <router-link>설정</router-link> -->
+    </div>
+  </div>
+</template>
+
+<style>
+#head-box {
+  width: 100%;
+  height: 50px;
+  /* min-width: 1000px; */
+  background-color: #6e8898;
+  display: flex;
+
+  /* 아이템 가운데 정렬 */
+  align-items: center;
+}
+
+/* 폰트 배경이 달라지는 문제 해결 */
+.font-background-transparency {
+  background-color: #ffffff00;
+}
+
+#navlink-home {
+  font-size: 2rem;
+
+  margin-left: 10px;
+}
+</style>
