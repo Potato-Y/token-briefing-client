bb411d20eafa432e43ad9490da8bb7acc0dc19e5
Potato-Y <projectpotatoy@gmail.com>
2022-09-11 15:49:36 +0900

    폰트 추가
    - 폰트 파일 추가
    - 전체 페이지에 기본 폰트를 적용
    - css 초기화

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,34 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-#app {
+@import "./css-reset.css"; /* css 초기화 */
+
+/* 나눔 스퀘어 굵음 */
+@font-face {
+  font-family: "NanumSquareRoundEB";
+  src: url("/src/assets/font/NanumSquareRoundEB.ttf");
+}
+
+/* 나눔 고딕 */
+@font-face {
+  font-family: "NanumGothic";
+  src: url("/src/assets/font/NanumGothic.otf");
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "NanumGothic" !important; /* 폰트 적용 */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+/* 기존 내용 삭제 */
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +49,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
