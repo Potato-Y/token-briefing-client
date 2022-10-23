1eb87b503a81f81a0a370ce6e1141f3cb16e1b5d
Potato-Y <projectpotatoy@gmail.com>
2022-10-24 00:24:13 +0900

    상태 관리 변경
    token briefing 과 memo에 대한 마지막 업데이트 정보를 저장할 상태 관리 저장소 생성

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,41 @@
 import { createStore } from 'vuex';
+import log from 'electron-log';
 
 export default createStore({
   state: {
     alertMsg: { msg: 'MSG', modalState: false },
+    /** 서버에서 받은 token briefing 데이터 */
+    tokenBriefingApiData: null,
+    /** 서버에서 받은 memo 데이터 */
+    memoApiData: null,
   },
   getters: {
     alertMsg: (state) => {
       return state.alertMsg;
     },
+    getTokenBriefingApiData: (state) => {
+      return state.tokenBriefingApiData;
+    },
+    getMemoApiData: (state) => {
+      return state.memoApiData;
+    },
   },
   mutations: {
     setAlert: (state, msg) => {
-      console.log('setAlert msg: ' + msg);
+      log.info('setAlert msg: ' + msg);
       state.alertMsg.msg = msg;
       state.alertMsg.modalState = true;
     },
     setAlertOff: (state) => {
       state.alertMsg.modalState = false;
+    },
+    setTokenBriefingApiData: (state, data) => {
+      log.info('setTokenBriefingApiData data: ' + data);
+      state.tokenBriefingApiData = data;
+    },
+    setMemoApiData: (state, data) => {
+      log.info('setMemoApiData data: ' + data);
+      state.memoApiData = data;
     },
   },
   actions: {},
