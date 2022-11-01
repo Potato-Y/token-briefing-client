0f921a28f6a182479d74b76e8b5404b16e8f2147
Potato-Y <projectpotatoy@gmail.com>
2022-11-01 21:37:26 +0900

    버전 정보, 오픈 소스 링크 추가

---
+++ src/views/About/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <div>버전: {{ appVer }}</div>
+
+    <div>
+      <router-link to="/opensource">오픈 소스 라이센스</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appVer: "로딩 중",
+    };
+  },
+  methods: {
+    getAppVer() {
+      const { ipcRenderer } = require("electron");
+      this.appVer = ipcRenderer.sendSync("api-get-app-version");
+    },
+  },
+  mounted() {
+    this.getAppVer();
+  },
+};
+</script>
